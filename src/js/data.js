277f95dbe2793a10de92170b2d29ee4c5fb53a28
Deadpool <mango.dev.opt@gmail.com>
2021-08-09 19:18:36 +0300

    update vacancies text, contact details

--- src/js/data.js
+++ src/js/data.js
@@ -1,19 +1,38 @@
 let data = {
-  QA1: {
-    icon: "./img/qa-icon.svg",
-    position: "QA Engineer Middle",
-    experience: "2-3 лет",
+  PRODUCT_DESIGNER: {
+    icon: "./img/des-icon.svg",
+    position: "Product (UI/UX) Designer",
+    experience: "4-5 лет",
     officeLocation: "Офис: г. Киев, Украина",
     collaborationVariant: "Офис",
-    buttonData: "QA1",
+    buttonData: "PRODUCT_DESIGNER",
     inside: {
       paragraph1: {
-        title: "Нужно знать наш технологический стек, который включает в себя:",
-        p: ["Manual testing", "Php", "Python"],
+        title: "Обязанности:",
+        p: [
+          "Разработка дизайна и дизайн-системы;",
+          "Создание прототипов/вайрфреймов;",
+          "Тестирование интерфейсов;",
+          "Изучение и внедрение практик UI/UX;",
+          "Сотрудничество с PO и BA."
+        ],
       },
       paragraph2: {
-        title: " Title-2",
-        p: ["Lorem", "Ipsum ", "is"],
+        title: "Что ждем от кандидата:",
+        p: [
+          "2+ года опыта в создании IT продуктов;",
+          "Опыт в  разработке дизайн-систем с нуля;",
+          "Понимание принципов UI/UX;",
+          "Знание современных трендов в дизайне.",
+        ],
+      },
+      paragraph3: {
+        title: "Будет плюсом:",
+        p: [
+          "Наличие портфолио на Behance/Dribbble;",
+          "Знание рынка аффилейт/рекламных сетей;",
+          "Опыт работы с Enterpirse/SaaS продуктами."
+        ],
       },
     },
   },
@@ -64,61 +83,57 @@ dataHide = {
       paragraph1: {
         title: "Нужно знать наш технологический стек, который включает в себя:",
         p: ["UI/UX", "Figma", "Photoshop"],
-      },
-      // paragraph2: {
-      //   title: " Title-2",
-      //   p: ["Lorem", "Ipsum ", "is"],
-      // },
+      }
     },
   },
-  FRONT2: {
-    icon: "./img/js-icon.svg",
-    position: "Front-end developer Junior",
-    experience: "1-2 года",
-    officeLocation: "Офис: г. Киев, Украина",
-    collaborationVariant: "Удаленное сотрудничество",
-    buttonData: "FRONT2",
-    inside: {
-      paragraph1: {
-        title: "Нужно знать наш технологический стек, который включает в себя:",
-        p: ["Js", "html/css", "Angular / Vue"],
-      },
-    },
-  },
-  UI2: {
-    icon: "./img/des-icon.svg",
-    position: "UI/UX Designer Middle",
-    experience: "4-6 лет",
-    officeLocation: "Офис: г. Киев, Украина",
-    collaborationVariant: "Офис",
-    buttonData: "UI2",
-    inside: {
-      paragraph1: {
-        title: "Нужно знать наш технологический стек, который включает в себя:",
-        p: ["UI/UX", "Figma"],
-      },
-      paragraph2: {
-        title: " Title-2",
-        p: ["Lorem", "Photoshop"],
-      },
-    },
-  },
-  test: {
-    icon: "./img/des-icon.svg",
-    position: "test",
-    experience: "4-6 лет",
-    officeLocation: "Офис: г. Киев, Украина",
-    collaborationVariant: "Офис",
-    buttonData: "test",
-    inside: {
-      paragraph1: {
-        title: "test:",
-        p: ["UI/UX", "Figma"],
-      },
-      paragraph2: {
-        title: " Title-2",
-        p: ["Lorem", "Photoshop"],
-      },
-    },
-  },
+  // FRONT2: {
+  //   icon: "./img/js-icon.svg",
+  //   position: "Front-end developer Junior",
+  //   experience: "1-2 года",
+  //   officeLocation: "Офис: г. Киев, Украина",
+  //   collaborationVariant: "Удаленное сотрудничество",
+  //   buttonData: "FRONT2",
+  //   inside: {
+  //     paragraph1: {
+  //       title: "Нужно знать наш технологический стек, который включает в себя:",
+  //       p: ["Js", "html/css", "Angular / Vue"],
+  //     },
+  //   },
+  // },
+  // UI2: {
+  //   icon: "./img/des-icon.svg",
+  //   position: "UI/UX Designer Middle",
+  //   experience: "4-6 лет",
+  //   officeLocation: "Офис: г. Киев, Украина",
+  //   collaborationVariant: "Офис",
+  //   buttonData: "UI2",
+  //   inside: {
+  //     paragraph1: {
+  //       title: "Нужно знать наш технологический стек, который включает в себя:",
+  //       p: ["UI/UX", "Figma"],
+  //     },
+  //     paragraph2: {
+  //       title: " Title-2",
+  //       p: ["Lorem", "Photoshop"],
+  //     },
+  //   },
+  // },
+  // test: {
+  //   icon: "./img/des-icon.svg",
+  //   position: "test",
+  //   experience: "4-6 лет",
+  //   officeLocation: "Офис: г. Киев, Украина",
+  //   collaborationVariant: "Офис",
+  //   buttonData: "test",
+  //   inside: {
+  //     paragraph1: {
+  //       title: "test:",
+  //       p: ["UI/UX", "Figma"],
+  //     },
+  //     paragraph2: {
+  //       title: " Title-2",
+  //       p: ["Lorem", "Photoshop"],
+  //     },
+  //   },
+  // },
 };
